d874dab59e715b4c41d8420f1e8d1175d868e65f
Michael Crump <mbcrump29@gmail.com>
2018-04-24 20:19:37 -0700

    first

--- twitterstats/Main.cs
+++ twitterstats/Main.cs
@@ -11,15 +11,9 @@ namespace twitterstats
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
 
-           
             UIApplication.Main(args, null, "AppDelegate");
-
-        
-           
+               
         }
-
-
-
-      
+              
     }
 }
